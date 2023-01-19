e416afd145b3825028735e8ace7e23467952916c
pharma71 <78262557+pharma71@users.noreply.github.com>
2023-01-19 12:24:09 +0100

    3 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ContentChildren, AfterContentInit, QueryList, ContentChild, ElementRef } from '@angular/core';
 import { Product } from '../products';
+import { ProductListComponent } from '../product-list/product-list.component'
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductAlertsComponent {
+export class ProductAlertsComponent implements AfterContentInit{
   @Input() product!: Product;
   @Output() notify = new EventEmitter();
+ // @ContentChildren('li') children!:QueryList<'li'>;
+  @ContentChild('p') child!:ElementRef
+  @ContentChildren(ProductListComponent,{ read:ElementRef }) myelem!:QueryList<ElementRef>
+
+  constructor(private elem:ElementRef){
+    
+  }
+
+  ngAfterContentInit(){
+  //  console.log(this.children.toString());
+    console.log((this.child.nativeElement as ElementRef));
+    console.log(this.myelem);
+  }
 }
